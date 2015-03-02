251e4e10fd5a9e198c98af5274e0d8e3fe28b676
Juan Antonio Galisteo <jgalisteo@nosolosoftware.biz>
2015-03-02 18:40:12 +0100

    Se admite el token en las cabeceras.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,12 +16,11 @@ class ApplicationController < ActionController::Base
   end
 
   def authenticate_user_from_token!
-    user_email = params[:user_email].presence
     user = User.find_by(email: user_email) if user_email
 
     # Notice how we use Devise.secure_compare to compare the token
     # in the database with the token given in the params, mitigating timing attacks
-    if user and Devise.secure_compare(user.authentication_token, params[:user_token])
+    if user and Devise.secure_compare(user.authentication_token, user_token)
       sign_in user, store: false
     end
   end
@@ -40,5 +39,15 @@ class ApplicationController < ActionController::Base
     respond_to do |format|
       format.all { render nothing: true, status: status }
     end
+  end
+
+  # Get the user email for authentication
+  def user_email
+    request.headers["HTTP_X_USER_EMAIL"] || params[:user_email]
+  end
+
+  # Get the user token for authentication
+  def user_token
+    request.headers["HTTP_X_USER_TOKEN"] || params[:user_token]
   end
 end
